5fbd0ead8a1ac28eb7e6b58d06704f1ef1de15a5
jaemin1005 <ghkdwoals1005@gmail.com>
2024-05-09 23:07:58 +0900

    추가: DOM Element 생성하는 클래스 작성
    CreateElement는 Element를 생성하는 클래스이다.
    create는 해당 element를 생성할 때, 매개 변수로 style, property를 받아
    생성된 Element에 적용한 후 해당 Element를 반환한다

--- Module/DataContext.ts
+++ Module/DataContext.ts
@@ -1,4 +1,4 @@
-import { IView } from "View/IVew";
+import { IView } from "Views/IVew";
 import { ViewModel } from "ViewModels/ViewModel";
 
 export const DataContext = function<T extends IView ,K extends ViewModel>(view : T, [...viewModel] : K[]){
